7c5a5198184e414c1ab5b76b96cdb19c1268ed09
thomasoca <thomas.ariasoca@gmail.com>
2021-09-06 19:52:59 +0700

    fix statuscmd merge

--- dwm.c
+++ dwm.c
@@ -484,7 +484,7 @@ buttonpress(XEvent *e)
 			arg.ui = 1 << i;
 		} else if (ev->x < x + blw)
 			click = ClkLtSymbol;
-		else if (ev->x > selmon->ww - (int)TEXTW(stext) - getsystraywidth()){
+		else if (ev->x > selmon->ww - statusw - getsystraywidth()){
 			x = selmon->ww - statusw;
 			click = ClkStatusText;
 			statussig = 0;
